34040493b45bf8cc601543f7986177cc17a84d01
Mario Magomedov <webdevelope2017@gmail.com>
2020-02-19 18:58:40 +0300

    section scills

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import '../styles/index.scss';
+import scrollbar from './scrollbar';
+import scrollbarAnimation from './scrollbarAnimation';
+
+window.onload = () => {
+  window.onscroll = function() {
+    if (window.scrollY > 300) {
+      scrollbar();
+      scrollbarAnimation();
+      window.scrollY = null;
+    }
+  };
+};
 
 console.log('webpack starterkit');
---
+++ src/scripts/scrollbarAnimation.js
@@ -0,0 +1,10 @@
+/**
+ * @function scrollbarAnimation
+ * @returns skillsRight class skills__right_top
+ */
+const scrollbarAnimation = () => {
+  let skillsRight = document.querySelector('.skills__right');
+  skillsRight.classList.add('skills__right_top');
+};
+
+export default scrollbarAnimation;
