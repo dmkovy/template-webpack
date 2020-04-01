9d38bcefad5d40d9f742ade4b4b2808068591df2
Mario <webdevelope2017@gmail.com>
2020-04-02 01:22:12 +0300

    scrolltop methods

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,35 @@ import './magnific-popup/index';
 import './lib/slick';
 
 window.onload = () => {
-  window.onscroll = function() {
-    if (window.scrollY > 300) {
+  window.onscroll = function(e) {
+    let winY = window.scrollY;
+    if (winY > 300) {
       // progress bar
       progressBar();
 
       scrollbarAnimation();
-      window.scrollY = null;
+
+      winY = null;
     }
   };
+
+  const scrollBtn = document.querySelector('.isShowBtn');
+
+  window.onscroll = () => {
+    if (window.scrollY > 700) {
+      scrollBtn.classList.remove('isShowBtn_hide');
+    } else if (window.scrollY < 700) {
+      scrollBtn.classList.add('isShowBtn_hide');
+    }
+  };
+
+  scrollBtn.onclick = () => {
+    window.scrollTo(0, 0);
+  };
+
+  // $('.isShowBtn').click(function() {
+  //   $(window).scrollTop(0);
+  // });
 };
 
 console.log('webpack starterkit');
